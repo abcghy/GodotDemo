2dc7d3d88138841908b0f5f8e8d887ea8c9a3281
Harry Gao <beatbox_gao@hotmail.com>
2020-02-18 15:35:55 +0800

    添加敌人，添加HUD

--- Panel.gd
+++ Panel.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	hide()
+	#hide()
 
 func _process(delta):
 	var velocity = Vector2()
@@ -35,3 +37,14 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
